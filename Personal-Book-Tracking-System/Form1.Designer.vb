c1a859ebe7d8350e58a9d0ab890d483a79705da2
= <=>
2025-09-23 00:31:51 +0800

    Add project files.

---
+++ Personal-Book-Tracking-System/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    Private components As System.ComponentModel.IContainer
+    Friend WithEvents dgvBooks As DataGridView
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnEdit As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents lblTitle As Label
+
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.dgvBooks = New System.Windows.Forms.DataGridView()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        CType(Me.dgvBooks, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvBooks
+        '
+        Me.dgvBooks.AllowUserToAddRows = False
+        Me.dgvBooks.AllowUserToDeleteRows = False
+        Me.dgvBooks.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dgvBooks.Location = New System.Drawing.Point(12, 50)
+        Me.dgvBooks.Name = "dgvBooks"
+        Me.dgvBooks.ReadOnly = True
+        Me.dgvBooks.Size = New System.Drawing.Size(616, 250)
+        Me.dgvBooks.TabIndex = 1
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(12, 310)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(200, 20)
+        Me.txtSearch.TabIndex = 2
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(386, 310)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 20)
+        Me.btnAdd.TabIndex = 4
+        Me.btnAdd.Text = "Add"
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Location = New System.Drawing.Point(469, 310)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(75, 20)
+        Me.btnEdit.TabIndex = 5
+        Me.btnEdit.Text = "Edit"
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(552, 310)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 20)
+        Me.btnDelete.TabIndex = 6
+        Me.btnDelete.Text = "Delete"
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(218, 310)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 3
+        Me.btnSearch.Text = "Search"
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Bold)
+        Me.lblTitle.Location = New System.Drawing.Point(12, 9)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(317, 25)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "📚 Personal Book Tracking System"
+        '
+        'Form1
+        '
+        Me.ClientSize = New System.Drawing.Size(640, 420)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.dgvBooks)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnEdit)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Name = "Form1"
+        Me.Text = "Book Tracking System"
+        CType(Me.dgvBooks, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+End Class
---
+++ Personal-Book-Tracking-System/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Personal-Book-Tracking-System")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Personal-Book-Tracking-System")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c489f9bf-195d-451a-ad1e-d5824e62ab63")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
